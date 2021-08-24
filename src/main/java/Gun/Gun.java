a01cb196834be6144f12c05e736d4c785b7f98cf
Times_New_Roman_ <65937168+Times-New-Roman@users.noreply.github.com>
2021-08-24 16:53:47 -0700

    Add interface methods

--- src/main/java/Gun/Gun.java
+++ src/main/java/Gun/Gun.java
@@ -1,4 +1,30 @@
 package Gun;
 
+import java.util.List;
+
 public interface Gun {
+
+    public int getDamage();
+
+    public int getHeadshotDamage();
+
+    public void setDamage();
+
+    public void setHeadshotDamage();
+
+    public default boolean canScope() {
+        return false;
+    }
+
+    public default boolean canAimDownSights() {
+        return true;
+    }
+
+    public default String getName() {
+        return "Gun";
+    }
+
+    public List<String> getLore();
+
+
 }
