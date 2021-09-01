9f7da0b4117fe8d89f036e70e38a574a1ff00a9e
Times-New-Roman <65937168+Times-New-Roman@users.noreply.github.com>
2021-09-01 11:39:30 -0700

    Refactor events

--- src/main/java/Event/EventListener.java
+++ src/main/java/Event/EventListener.java
@@ -1,16 +1,23 @@
 package Event;
 
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class UtilityUseEvent implements Listener {
+public class EventListener implements Listener {
 
+    @EventHandler
     public void onUtilityUse(PlayerInteractEvent e) {
         if (!e.getAction().equals(Action.RIGHT_CLICK_AIR) || !e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) { return; }
         else if (e.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.EMERALD)) {
 
         }
+    }
+
+    @EventHandler
+    public void onWeaponFire(PlayerInteractEvent e) {
+        if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || !e.getAction().equals(Action.RIGHT_CLICK_AIR))
     }
 }
